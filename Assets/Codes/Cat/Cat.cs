7e6a5941fd6dafd7d4858c20a0b4bd66472a6fe5
OwenPi314 <owenpi3.1415922@gmail.com>
2021-10-02 00:32:52 -0700

    huh

--- Assets/Codes/Cat/Cat.cs
+++ Assets/Codes/Cat/Cat.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Cat : MonoBehaviour
 {
     private int currHealth;
-    public HealthBar hb;
+    public HealthBar HB;
+    public SpriteRenderer SR;
+    public DateTime disappear = DateTime.Now;
 
     // Start is called before the first frame update
     void Start()
@@ -14,16 +17,49 @@ public class Cat : MonoBehaviour
         currHealth = 9;
     }
 
+    void OnCollisionEnter2D(Collision2D col)        
+    {
+        float num = UnityEngine.Random.value;
+        var currTime = System.DateTime.Now;
+
+
+        if(num < 0.5)
+        {
+            disappear = DateTime.Now;
+            SR.enabled = false;
+            currHealth--;
+        }  
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if((DateTime.Now - disappear).Seconds > 3)
+        {
+            SR.enabled = true;
+        }
+
+        HB.setHealth(currHealth);
 
         // testing heathbar
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKey(KeyCode.LeftArrow))
         {
-            currHealth--;
-            hb.setHealth(currHealth);
+            transform.Translate(-150 * Time.deltaTime, 0, 0);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(150 * Time.deltaTime, 0, 0);
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -150 * Time.deltaTime, 0);
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 150 * Time.deltaTime, 0);
         }
     }
-
 }
