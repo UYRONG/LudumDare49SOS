38c2788546b80094c72e74c06b648adcebee40a4
OwenPi314 <owenpi3.1415922@gmail.com>
2021-10-01 17:32:17 -0700

    Working health bar

--- Assets/Codes/Cat/Cat.cs
+++ Assets/Codes/Cat/Cat.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Cat : MonoBehaviour
 {
+    private int currHealth;
+    public HealthBar hb;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {        
+        // testing heathbar
+        currHealth = 9;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // testing heathbar
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            currHealth--;
+            hb.setHealth(currHealth);
+        }
     }
+
 }
---
+++ Assets/Codes/Cat/HealthBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void setHealth(int health)
+    {
+        slider.value = health;
+    }
+}
