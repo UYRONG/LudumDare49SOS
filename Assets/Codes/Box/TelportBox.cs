abe3d3ad717043589662c730b93db20616c23411
Hehuimin Cheng <hehuimin.cheng@mail.mcgill.ca>
2021-10-02 15:43:37 -0400

    Added Fish and More stuff

--- Assets/Codes/Box/TelportBox.cs
+++ Assets/Codes/Box/TelportBox.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class TelportBox : Box
 {
 
+    public Vector2 teleportPosition;
+
         public override void interact(Cat c){
             float chance = Utils.generateRandom();
             if(chance > 0.5f){
-                c.teleport(new Vector2(0,0));
+                c.teleport(teleportPosition);
             }
         }
 }
