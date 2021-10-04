bb775210ed3b1822601424c1ac1e3a8ddb7b55ec
Zoraaaa <rongyu923826@outlook.com>
2021-10-04 04:56:29 -0400

    added ground, bottle prefab, background pics

---
+++ Assets/Codes/Interactive/Can.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Can : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Cat cat = col.gameObject.GetComponent<Cat>();
+        cat.pickFish();
+        cat.pickFish();
+        cat.pickFish();
+        Destroy(this.gameObject);
+    }
+}
+
---
+++ Assets/Codes/Spray_Bottle/sprayBottleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sprayBottleController : MonoBehaviour
+{
+    System.DateTime lastActionTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastActionTime = System.DateTime.Now;
+        Debug.Log("GIAO");
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if ((System.DateTime.Now - lastActionTime).TotalSeconds > 1){
+            Debug.Log("GIAO");
+            lastActionTime = System.DateTime.Now;
+            Cat c = col.gameObject.GetComponent<Cat>();
+            c.reduceHealth();
+        }
+    }
+
+}
