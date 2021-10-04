33137333febe328b597790c7b4557329aad6fb09
Zoraaaa <rongyu923826@outlook.com>
2021-10-04 14:55:39 -0400

    modified scenes

--- Assets/Codes/Box/TelportBox.cs
+++ Assets/Codes/Box/TelportBox.cs
@@ -9,7 +9,7 @@ public class TelportBox : Box
 
         public override void interact(Cat c){
             float chance = Utils.generateRandom();
-            if(chance > 0.0f){
+            if(chance > 0.5f){
                 c.teleport(teleportPosition);
             }
         }
