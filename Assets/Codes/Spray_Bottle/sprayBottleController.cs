b22e660081b713ebbacec2b40ae3a36f7ca3ecbb
Hehuimin Cheng <hehuimin.cheng@mail.mcgill.ca>
2021-10-04 12:47:16 -0400

    Add Button

--- Assets/Codes/Spray_Bottle/sprayBottleController.cs
+++ Assets/Codes/Spray_Bottle/sprayBottleController.cs
@@ -9,13 +9,11 @@ public class sprayBottleController : MonoBehaviour
     void Start()
     {
         lastActionTime = System.DateTime.Now;
-        Debug.Log("GIAO");
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
         if ((System.DateTime.Now - lastActionTime).TotalSeconds > 1){
-            Debug.Log("GIAO");
             lastActionTime = System.DateTime.Now;
             Cat c = col.gameObject.GetComponent<Cat>();
             c.reduceHealth();
